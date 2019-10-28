4dd17bd58bbd23a3378a3c480238e8cfc5570a52
Yuriy Zahreva <yura.zahreva@gmail.com>
2019-10-28 23:01:19 +0200

    :pencil2: [database/helpers/bots] Fixed typo in model name

--- src/database/helpers/bots/destroy.js
+++ src/database/helpers/bots/destroy.js
@@ -1,8 +1,8 @@
-const db = require('#database');
+const { Bots } = require('#database');
 const AppError = require('#error');
 
 function destroy(id) {
-  return db.bots.destroy({ where: { id } }).catch(err => {
+  return Bots.destroy({ where: { id } }).catch(err => {
     throw new AppError({
       code: AppError.ERROR_CODES.DB_ERROR,
       message: err.message || AppError.MESSAGES.DEFAULT
